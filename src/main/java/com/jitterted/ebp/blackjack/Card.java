c923e17ef94d83cf1cb584c84ce70f1b169234a4
Ted M. Young <tedyoung@gmail.com>
2021-11-08 15:40:36 -0800

    LAB #4: Completed switch to use Suit from the outside.

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -8,9 +8,8 @@ public class Card {
     private final Suit suit;
     private final String rank;
 
-    // Card(Suit suit, String rank)
-    public Card(String symbol, String rank) {
-        this.suit = Suit.from(symbol);
+    public Card(Suit suit, String rank) {
+        this.suit = suit;
         this.rank = rank;
     }
 
@@ -44,20 +43,26 @@ public class Card {
 
     @Override
     public String toString() {
-        return "Card {" +
+        return "Card{" +
                 "suit=" + suit +
-                ", rank=" + rank +
+                ", rank='" + rank + '\'' +
                 '}';
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Card card = (Card) o;
 
-        if (!suit.equals(card.suit)) return false;
+        if (suit != card.suit) {
+            return false;
+        }
         return rank.equals(card.rank);
     }
 
